4090a40744ca91490761d17b4b4569543b96917f
floppiSeaL <xs1115@nyu.edu>
2021-04-21 22:27:32 +0800

    a change

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+let data;
+let poses;
+let a = [8,8,6,12,12,11,6,7,7,11];
+let b = [10,6,12,14,11,13,5,5,9,5];
+
+function preload(){
+  data = loadJSON("pose.json");
+ }
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(200);
+
+  for (let i = 0; i < data.bodypoints.length; i++) {
+    let poses= data.bodypoints;
+    let posX = poses[i].x;
+    let posY = poses[i].y;
+    fill(0)
+    stroke(10)
+    circle(posX,posY,10)
+    let name = poses[i].part;
+    text(name, posX-20,posY-20)
+   
+  }
+    let x1 = poses[a].x;
+    let y1 = poses[a].y;
+    let x2 = poses[b].x;
+    let y2 = poses[b].y;
+
+    stroke(0)
+    strokeWeight(10)
+    line(x1,y1,x2,y2)
+  
 }
+
